eb0d898fd577667d5cebcae5c95ea5542ae6d112
markankaro <markankaro2016@gmail.com>
2018-04-15 18:23:58 +0200

    Update models.py

--- gamecatalog/models.py
+++ gamecatalog/models.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import date
+
 from django.contrib.auth.models import User
 from django.db import models
-from datetime import date
 
 
 # Create your models here.
@@ -12,18 +13,23 @@ class Game(models.Model):
     name = models.TextField()
     company = models.ForeignKey(Company, related_name='developed by')
     genre = models.ForeignKey(Genre, related_name='of the genre')
+    description = models.TextField(blank=True, null=True)
+    series = models.ForeignKey(blank=True, null=True)
 
 
 class Company(models.Model):
     name = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
 
 class Platform(models.Model):
     name = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
 
 class Genre(models.Model):
     name = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
 
 class GameRelease(models.Model):
@@ -32,6 +38,8 @@ class GameRelease(models.Model):
     platform = models.ForeignKey(Platform)
     cover = models.ImageField(upload_to='img/covers', black='true')
     date = models.DateField(default=date.today)
+    description = models.TextField(blank=True, null=True)
+
     class Meta:
         unique_together = ("game", "platform")
 
@@ -42,6 +50,13 @@ class GameReview(models.Model):
     RATING_CHOICES = ((0, 'zero'), (1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'), (6, 'six'), \
                       (7, 'seven'), (8, 'eight', (9, 'nine'), (10, 'ten')))
     rating = models.PositiveSmallIntegerField('Rating (0-10)', blank=False, default=3, choices=RATING_CHOICES)
+    review = models.TextField(blank=True, null=True)
     date = models.DateField(default=date.today)
+
     class Meta:
         unique_together = ("game", "user")
+
+
+class Series(models.Model):
+    name = models.TextField()
+    description = models.TextField(blank=True, null=True)

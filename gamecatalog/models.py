9e24ccdfe74ea2c9e97cd2fc743f3776cf3187da
markankaro <markankaro2016@gmail.com>
2018-04-15 17:32:46 +0200

    Start models.py

--- gamecatalog/models.py
+++ gamecatalog/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+from datatime import datetime
+
+class Games(models.Model):
+    name = models.TextField()
+    company =  models.ForeignKey(Company, related_name='developed_by')
+
+class Company(models.Model):
+    name = models.TextField()
+
+class Plataform(models.Model):
+    name = models.TextField()
+
+class Genre(models.Model):
+    name = models.TextField()

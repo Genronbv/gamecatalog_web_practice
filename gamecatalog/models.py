f90ea9e5e023f0032871adb72d29dc7081065f12
markankaro <markankaro2016@gmail.com>
2018-04-15 18:35:06 +0200

    models.py working

--- gamecatalog/models.py
+++ gamecatalog/models.py
@@ -9,22 +9,17 @@ from django.db import models
 
 # Create your models here.
 
-class Game(models.Model):
+class Series(models.Model):
     name = models.TextField()
-    company = models.ForeignKey(Company, related_name='developed by')
-    genre = models.ForeignKey(Genre, related_name='of the genre')
     description = models.TextField(blank=True, null=True)
-    series = models.ForeignKey(blank=True, null=True)
 
 
 class Company(models.Model):
     name = models.TextField()
     description = models.TextField(blank=True, null=True)
-
-
-class Platform(models.Model):
-    name = models.TextField()
-    description = models.TextField(blank=True, null=True)
+    director = models.TextField(blank=True, null=True)
+    city = models.TextField(blank=True, null=True)
+    web = models.TextField(blank=True, null=True)
 
 
 class Genre(models.Model):
@@ -32,11 +27,24 @@ class Genre(models.Model):
     description = models.TextField(blank=True, null=True)
 
 
+class Game(models.Model):
+    name = models.TextField()
+    company = models.ForeignKey(Company, related_name='developed_by')
+    genre = models.ForeignKey(Genre, related_name='of_the_genre')
+    description = models.TextField(blank=True, null=True)
+    series = models.ForeignKey(Series, blank=True, null=True)
+
+
+class Platform(models.Model):
+    name = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField(default=date.today)
+
+
 class GameRelease(models.Model):
     game = models.ForeignKey(Game)
-    date = models.DateField()
     platform = models.ForeignKey(Platform)
-    cover = models.ImageField(upload_to='img/covers', black='true')
+    cover = models.ImageField(upload_to='img/covers', blank='true')
     date = models.DateField(default=date.today)
     description = models.TextField(blank=True, null=True)
 
@@ -45,18 +53,14 @@ class GameRelease(models.Model):
 
 
 class GameReview(models.Model):
-    game = models.ForeignKey(Game, related_name='review of')
-    user = models.ForeignKey(User, related_name='reviewed by')
-    RATING_CHOICES = ((0, 'zero'), (1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'), (6, 'six'), \
-                      (7, 'seven'), (8, 'eight', (9, 'nine'), (10, 'ten')))
+    game = models.ForeignKey(Game, related_name='review_of')
+    user = models.ForeignKey(User, related_name='reviewed_by')
+    RATING_CHOICES = (
+    (0, 'zero'), (1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'), (6, 'six'), (7, 'seven'),
+    (8, 'eight'), (9, 'nine'), (10, 'ten'))
     rating = models.PositiveSmallIntegerField('Rating (0-10)', blank=False, default=3, choices=RATING_CHOICES)
     review = models.TextField(blank=True, null=True)
     date = models.DateField(default=date.today)
 
     class Meta:
         unique_together = ("game", "user")
-
-
-class Series(models.Model):
-    name = models.TextField()
-    description = models.TextField(blank=True, null=True)
